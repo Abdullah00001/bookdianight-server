05c4e0d7cd0279f2e4822aaf788e46c92c55dcfd
Abdullah00001 <abdullahbinomarchowdhury@gmail.com>
2026-08-07 12:02:31 +0600

    chore : scafolding and project setup with scheduler service

---
+++ scheduler/src/app/jobs/index.ts
@@ -0,0 +1,12 @@
+import logger from '@/app/configs/logger.configs';
+
+// ---------------- Import Job Here ----------------
+
+
+
+
+// ---------------- Register Job Here ----------------
+
+
+
+logger.info('[Corn] Schedules registered');
